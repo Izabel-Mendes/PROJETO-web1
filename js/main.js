0a7c3624311a1133c705067020d18df52e2ec8ed
Izabel Mendes <mendes.izabel08@gmail.com>
2022-01-18 19:42:41 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-$('.slider-main').slick();
+$('.slider-main').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+
+});
+function validarEmail() {
+    var email =
+        document.querySelector('#email');
+    var error = document.querySelector('#error-email');
+
+    if (!email.checkValidity()) {
+        error.innerHTML = "Email inválido";
+    }
+
+}
+function redefinirMsg() {
+    var error = document.querySelector('#error-email');
+    if (error.innerHTML == "Email invalido") {
+        error.innerHTML = "";
+    }
+}
+
+function mascara_cpf() {
+    var cpf = document.getElementById('cpf')
+    if (cpf.value.length == 3 || cpf.value.length == 7) {
+        cpf.value += "."
+    } else if (cpf.value.length == 11) {
+        cpf.value += "-"
+    }
+}
+
